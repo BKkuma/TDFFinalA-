681db8a476694afaed0c3fc1701619433437ac67
Nattawut <Bank578974@gmail.com>
2024-05-02 22:40:46 +0700

    Path3 90 Percent

--- Assets/Script/Node.cs
+++ Assets/Script/Node.cs
@@ -7,9 +7,26 @@ public class Node
     public bool walkable;
     public Vector3 worldPosition;
 
-    public Node(bool _walkable, Vector3 _wolrdPos)
+    public int gridX;
+    public int gridY;
+
+    public int gCost;
+    public int hCost;
+    public Node parent;
+
+    public Node(bool _walkable, Vector3 _wolrdPos,  int _gridX, int _gridY)
     {
         walkable = _walkable;
         worldPosition = _wolrdPos;
+        gridX = _gridX;
+        gridY = _gridY;
+    }
+
+    public int fCost
+    {
+        get
+        {
+            return gCost + hCost;
+        }
     }
 }
